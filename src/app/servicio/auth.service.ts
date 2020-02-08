12d1274c9d94302b40a0ba6ed1f026c1db68e2d9
NelsonSosa <nelsonrsavallone@gmail.com>
2020-02-08 12:24:48 -0300

    Commit nelson

---
+++ src/app/servicio/auth.service.ts
@@ -0,0 +1,75 @@
+import { UserInterface } from './../model/user';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
+import { auth } from 'firebase/app';
+import { error } from 'util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afsAuth: AngularFireAuth, private afs: AngularFirestore) { }
+//---------------------------------------------------REGISTER USER---------------------------------
+  registerUser(email: string, pass: string) {
+    // tslint:disable-next-line: no-shadowed-variable
+    return new  Promise ((resolve, reject ) => {
+      this.afsAuth.auth.createUserWithEmailAndPassword(email, pass).
+      then(userData => {
+         resolve (userData), this.updateUserDate(userData.user); })
+      .catch(err => {reject(err), alert(err.message); });
+    });
+  }
+  //--------------------------------------------------EMAIL Y CONTRASENIA------------------------
+  loginEmailUser(email: string, pass: string) {
+    // tslint:disable-next-line: no-shadowed-variable
+    return new Promise((resolve, reject ) => {
+      this.afsAuth.auth.signInWithEmailAndPassword(email, pass)
+      .then(userData => resolve(userData), err => {reject(err), alert(err.message); });
+    });
+  }
+  //---------------------------------------------------FACEBOOK LOGIN---------------------------
+  loginFacebookUser() {
+   return this.afsAuth.auth.signInWithPopup(new auth.FacebookAuthProvider())
+   .then((credential) => {
+     this.updateUserDate(credential.user);
+   });
+
+  }
+  //----------------------------------------------------GOOGLE LOGIN----------------------------
+  loginGoogleUser() {
+    return this.afsAuth.auth.signInWithPopup(new auth.GoogleAuthProvider())
+    .then((credential) => {
+      this.updateUserDate(credential.user);
+    });
+
+
+  }
+  //-----------------------------------------------------LOGOUT----------------------------------
+  logoutUser() {
+    return this.afsAuth.auth.signOut();
+  }
+
+  isAuth() {
+    // tslint:disable-next-line: no-shadowed-variable
+    return this.afsAuth.authState.pipe(map(auth => auth));
+  }
+
+private updateUserDate(user) {
+  const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+  const data: UserInterface = {
+    id : user.uid,
+    email: user.email,
+    roles: {
+      admin: true
+    }
+  };
+  return userRef.set(data, {merge: true});
+}
+isUserAdmin(userUid) {
+  return this.afs.doc<UserInterface>(`users/${userUid}`).valueChanges();
+
+}
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+   firebaseConfig: {
+    apiKey: "AIzaSyDmDUbSsU91usUM9bz1En95Q1Lk4pbZ_QQ",
+    authDomain: "bookstore-df532.firebaseapp.com",
+    databaseURL: "https://bookstore-df532.firebaseio.com",
+    projectId: "bookstore-df532",
+    storageBucket: "bookstore-df532.appspot.com",
+    messagingSenderId: "90441209735",
+    appId: "1:90441209735:web:a12fc7a9dcdd8da016a522",
+    measurementId: "G-57BPS6MF8G"
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
